0ffbd9c5717839d048c7ae5854ffad9340fb3651
Ayan Hazra <ayan.hazra1901@gmail.com>
2026-09-07 02:35:11 +0530

    feat: add dynamic glass scroll effect to topbar and refine layout glass styles

--- frontend/src/components/DashboardLayout.jsx
+++ frontend/src/components/DashboardLayout.jsx
@@ -11,10 +11,10 @@ const DashboardLayout = () => {
       <Sidebar />
       
       {/* Main Content Area (offset by left column width on desktop) */}
-      <div className="flex-1 flex flex-col min-h-screen ml-0 lg:ml-56 transition-all">
+      <div className="flex-1 flex flex-col min-h-screen ml-0 lg:ml-56 transition-all relative">
         <Topbar />
         
-        <main className="flex-1 p-3 sm:p-5 lg:p-8 overflow-y-auto bg-slate-50/70 pb-12">
+        <main className="flex-1 p-3 sm:p-5 lg:p-8 bg-slate-50/70 pb-12">
           <Outlet />
         </main>
       </div>
